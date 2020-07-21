85e1889e46637f2bf39d41a83f71f0080f01f733
Honzin <xsedaj00@gmail.com>
2020-07-21 12:27:38 +0200

    v0.3.3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ description = "Flexible Network"
 
 setup(
     name='flexet',
-    version='0.3.2',
+    version='0.3.3',
     packages=find_packages(),
     url='https://github.com/ShadowCodeCz/flexet',
     project_urls={
